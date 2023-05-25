85846b91efc2480fc94f676ed185616c6d6f3e9a
Jesus Alfonso <jesus.alfonso@ecommfarm.com>
2023-05-26 01:28:38 +0200

    update composer.json

--- src/NotificationsServiceProvider.php
+++ src/NotificationsServiceProvider.php
@@ -13,14 +13,12 @@ class NotificationsServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/notifications'),
         ], 'views');
-
-        $this->publishes([
-            __DIR__.'/../config/notifications.php' => config_path('notifications.php'),
-        ], 'config');
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/notifications.php', 'notifications');
+        $this->app->singleton('notifications', function () {
+            return new Notifications();
+        });
     }
 }
