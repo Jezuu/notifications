1e60dbe180953e0b9c6464874f576c7c386fb8c2
Jesus Alfonso <ext-jesus.ruiz@imdea.org>
2023-05-26 09:48:05 +0200

    Updates

--- src/NotificationsServiceProvider.php
+++ src/NotificationsServiceProvider.php
@@ -8,17 +8,19 @@ class NotificationsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'notifications');
+        //$this->loadViewsFrom(__DIR__ . '/../resources/views', 'notifications');
 
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/notifications'),
         ], 'views');
+
+        $this->publishes([
+            __DIR__.'/../config/notifications.php' => config_path('notifications.php'),
+        ], 'config');
     }
 
     public function register()
     {
-        $this->app->singleton('notifications', function () {
-            return new Notifications();
-        });
+        $this->mergeConfigFrom(__DIR__.'/../config/notifications.php', 'notifications');
     }
 }
