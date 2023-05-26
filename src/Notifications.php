5ec6ecc498d40b5931c4e3f9b596cb6a8da41465
Jesus Alfonso <ext-jesus.ruiz@imdea.org>
2023-05-26 15:59:44 +0200

    Updates

--- src/Notifications.php
+++ src/Notifications.php
@@ -4,8 +4,19 @@ namespace Notifications;
 
 class Notifications
 {
+    public static function send($type, $message)
+    {
+        session()->flash($type, $message);
+        return new self();
+    }
+
     public static function notify($type, $message)
     {
         return view('notifications::notification', compact('type', 'message'));
+    }
+
+    public static function view($view)
+    {
+        return view($view);
     }
 }
